f9f1264e44204b76607ffd6eba22c05639fb3753
saikiranreddy <saikiranreddyenugala@gmail.com>
2021-03-24 23:38:47 -0500

    Update compute.js

--- routes/compute.js
+++ routes/compute.js
@@ -3,11 +3,11 @@ var express=require('express')
 var router=express.Router();
 
 router.get('/',(req,res)=>{
-    let randomNumber=(Math.random()*100)+1
+    let randomNo=(Math.random()*100)+1
     let data=req.query.x
     console.log(data)
     res.render('compute',{
-        randSolution:`ceil applied to ${randomNumber} is ${Math.ceil(randomNumber)}`,
+        randSolution:`ceil applied to ${randomNo} is ${Math.ceil(randomNo)}`,
         querySolution:` ceil applied to ${data} is ${Math.ceil(data)}`
     })
 })
