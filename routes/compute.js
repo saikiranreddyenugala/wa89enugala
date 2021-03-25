013fd96bb457c7d9731bbf9153074898e2e73963
saikiranreddy <saikiranreddyenugala@gmail.com>
2021-03-24 23:34:57 -0500

    compute

--- routes/compute.js
+++ routes/compute.js
@@ -3,12 +3,12 @@ var express=require('express')
 var router=express.Router();
 
 router.get('/',(req,res)=>{
-    let randomNumber=Math.floor(Math.random()*100)+1
+    let randomNumber=(Math.random()*100)+1
     let data=req.query.x
     console.log(data)
     res.render('compute',{
-        randSolution:`cos applied to ${randomNumber} is ${Math.cos(randomNumber)}`,
-        querySolution:` cos applied to ${data} is ${Math.cos(data)}`
+        randSolution:`ceil applied to ${randomNumber} is ${Math.ceil(randomNumber)}`,
+        querySolution:` ceil applied to ${data} is ${Math.ceil(data)}`
     })
 })
 
