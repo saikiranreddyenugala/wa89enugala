74ef0dd758b4da91c9918d01d1fab19166c5dd59
saikiranreddy <saikiranreddyenugala@gmail.com>
2021-03-24 23:27:52 -0500

    Added compute

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET Mydata page. */
 router.get('/', function(req, res, next) {
-  res.render('mydata', { title: 'Jeevan Bodigam' });
+  res.render('mydata', { title: 'saikiran reddy enugala' });
 });
 
 module.exports = router;
